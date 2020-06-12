d0b3ffe119749ec384344c91ca87153c09230ac6
Kevin Garcia <kg.gar91@gmail.com>
2020-06-12 19:14:36 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -37,11 +37,8 @@ def sorted_matrix
   # However, this time, sort each internal array alphabetically by the first character
 end
 
-def matrix_lookup(produce, 1, 1)
-  produce = [
-    ["Eggplant", "Grapefruit", "Oranges", "Pineapple", "Watermelon"],
-    ["Asparagus", "Avocadoes", "Grapes", "Potatoes", "Strawberries"]
-  ]
+def matrix_lookup(matrix, row, column)
+  matrix[0][1]
   # Given any matrix (array of arrays), a row index and a column index,
   # Return the matrix's content at that row and and column
 end
